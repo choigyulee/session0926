1a33ae0e2c3ec1102ebf5b9037a1b11f916f569d
choigyulee <cgl1409@gmail.com>
2024-10-02 15:45:00 +0900

    session 과제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Join from './components/Join'
+import styled from 'styled-components'
+import SignUp from './components/SignUp'
 import Login from './components/Login'
 
+
 function App() {
-  return (
-    <div>
-      <Join />
-      <Login />
-    </div>
-  );
+
+return (
+  <Container>
+    <SignUp />
+    <Login />
+  </Container>
+);
 }
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+`
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,61 +1,79 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const ServerURL = process.env.REACT_APP_SERVER_URL;
 
-
-function Login() {
+const SignUp = () => {
 
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
-  
-  const handleLogin = async (e) => {
-      try {
-          const response = await axios.post(`${ServerURL}/api/user/login`, {
-              userId,
-              password,
-            })
-            if (response.status === 200) {
-              alert(`로그인 성공, 사용자 고유 id: ${response.data}`)
-            }
-          } catch (error) {
-            alert('로그인에 실패했습니다.')
-          }
-        }
+  const [email, setEmail] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post(`${ServerURL}/api/user/signup`, {
+        userId,
+        password,
+        email,
+      })
+      if (response.status === 200) {
+        alert('회원가입이 완료되었습니다.')
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        alert('이미 사용 중인 아이디입니다.')
+      } else {
+        alert('회원가입에 실패하였습니다.')
+      }
+    };
+  }
 
 
 return (
     <SubContainer>
-      <Title>로그인</Title>
+      <Title>회원가입</Title>
       <Item>
+        <ItemLabel>아이디<RequireSpan>*</RequireSpan></ItemLabel>
         <InputDiv>
           <InputField
             type='text'
             id='userId'
             value={userId}
-            onChange={e => setUserId(e.target.value)}
-            placeholder="아이디"
+            onChange={e => setUserId(e.target.value)} 
+            placeholder="아이디를 설정해주세요"
             required />
         </InputDiv>
       </Item>
       <Item>
+        <ItemLabel>비밀번호<RequireSpan>*</RequireSpan></ItemLabel>
         <InputDiv>
           <InputField
             type='password'
             id='password'
             value={password}
             onChange={e => setPassword(e.target.value)}
-            placeholder="비밀번호"
+            placeholder="비밀번호를 설정해주세요"
             required />
         </InputDiv>
       </Item>
-      <Button onClick={handleLogin}>로그인 완료</Button>
+      <Item>
+        <ItemLabel>이메일</ItemLabel>
+        <InputDiv>
+          <InputField
+            type='email'
+            id='email'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="이메일을 입력해주세요"/>
+        </InputDiv>
+      </Item>
+      <Button onClick={handleSignUp}>회원가입 완료</Button>
     </SubContainer>
 );
 }
 
-export default Login;
+export default SignUp;
 
 
 const SubContainer = styled.div`
@@ -78,6 +96,14 @@ const Item = styled.div`
   margin-bottom: 1.2rem;
 `
 
+const ItemLabel = styled.label`
+  font-size: 20px;
+`
+
+const RequireSpan = styled.span`
+  color: red;
+`
+
 const InputDiv = styled.div`
   position: relative;
   align-items: center;
@@ -89,7 +115,7 @@ const InputField = styled.input`
   border: 2px solid #737373;
   border-radius: 10px;
   box-sizing: border-box;
-  margin: 2px 0;
+  margin: 8px 0;
   font-size: 20px;
   font-weight: 400;
   padding-left: 20px;
